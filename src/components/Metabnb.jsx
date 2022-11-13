c19917c7048b346553627c231d44170549909db6
Favour-Cell <favourenwonwukc@gmail.com>
2022-11-13 06:03:03 +0100

    fixed gh issue

--- src/components/Metabnb.jsx
+++ src/components/Metabnb.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import boxes from '../assets/3boxes.png'
 
 const Metabnb = () => {
   return (
-    <div className='flex w-full justify-center md:justify-between mt-20 px-4 md:px-10 lg:px-24'>
-
+    <div className='flex w-full justify-center md:justify-between mt-20 px-4 md:px-10 lg:px-24 bg-[#A02279]'>
+        <div></div>
+        <div>
+            <img src={boxes} alt="" />
+        </div>
     </div>
   )
 }
