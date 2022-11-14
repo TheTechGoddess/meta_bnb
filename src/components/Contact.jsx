8cd9f7f944f7c9de68e3af9777e371932d35dd94
Favour-Cell <favourenwonwukc@gmail.com>
2022-11-14 17:58:42 +0100

    updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,9 +7,8 @@ import {FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa'
 
 const Contact = () => {
   return (
-    <div className='flex w-full py-10 justify-between px-4 md:px-10 lg:px-24 bg-black text-[#FFFFFF] content-center'>
-        <div className='flex sm:flex-col'>
-        <div className=''>
+    <div className='md:flex w-full py-10 md:justify-between px-4 md:px-10 lg:px-24 bg-black text-[#FFFFFF] content-center'>
+        <div className='flex justify-between md:justify-start md:flex-col'>
             <div className='flex mb-20'>
                 <img src={house2} alt="" className='w-8' />
                 <img src={logo2} alt="" className='w-24' />
@@ -20,33 +19,30 @@ const Contact = () => {
                 <FaTwitter className='ml-4 text-sm'/>
             </div>
             <div className='flex'>
-                <img src={copyright} alt="" />
+                <img src={copyright} alt="" className='w-5 h-5' />
                 <p className='ml-3'>2022 Metabnb</p>
             </div>
         </div>
-        <div>
+        <div className='flex justify-between md:justify-start md:flex-col'>
             <p className='font-bold pb-4'>Community</p>
             <p className='text-sm py-1'>NFT</p>
             <p className='text-sm py-1'>Tokens</p>
             <p className='text-sm py-1'>Landlords</p>
             <p className='text-sm py-1'>Discord</p>
         </div>
-        </div>
-        <div className='flex sm:flex-col'>
-        <div>
+        <div className='flex justify-between md:justify-start md:flex-col'>
             <p className='font-bold pb-4'>Places</p>
             <p className='text-sm py-1'>Castle</p>
             <p className='text-sm py-1'>Farm</p>
             <p className='text-sm py-1'>Beach</p>
             <p className='text-sm py-1'>Learn more</p>
         </div>
-        <div>
+        <div  className='flex justify-between md:justify-start md:flex-col'>
             <p className='font-bold pb-4'>About us</p>
             <p className='text-sm py-1'>Road map</p>
             <p className='text-sm py-1'>Creators</p>
             <p className='text-sm py-1'>Career</p>
             <p className='text-sm py-1'>Contact us</p>
-        </div>
         </div>
     </div>
   )
