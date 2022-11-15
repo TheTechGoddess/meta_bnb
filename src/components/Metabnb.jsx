4a6d676a5edfc27a7b4123e52a41c32afe5e2e46
Favour-Cell <favourenwonwukc@gmail.com>
2022-11-15 13:27:38 +0100

    updates

--- src/components/Metabnb.jsx
+++ src/components/Metabnb.jsx
@@ -8,7 +8,14 @@ const Metabnb = () => {
         <div className='text-center object-center flex justify-center content-center pb-16 md:pb-0'>
             <img src={boxes} alt="" className='w-80 md:w-[600px] self-center' />
         </div>
-        <div className='md:mt-20 flex flex-col justify-center content-center text-center md:text-start'>
+        <div className='flex flex-col md:hidden justify-center content-center text-center'>
+        <h1 className='text-5xl font-extrabold pb-10 '>Metabnb NFTs</h1>
+            <p className='text-lg px-16 mb-12 '>
+            Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our customer access to loads of our excluysive services.
+            </p>
+            <button className='bg-white text-[#A02279] px-8 w-[150px] py-2 self-center'>Learn more</button>
+        </div>
+        <div className='hidden md:flex mt-20 flex-col justify-center content-center text-start'>
             <h1 className='text-5xl font-extrabold pb-10 '>Metabnb NFTs</h1>
             <p className='text-lg tracking-wide'>Discover our NFT gift cards collection. Loyal </p>
             <p className='text-lg tracking-wide'>customers gets amazing gift cards which are</p>
